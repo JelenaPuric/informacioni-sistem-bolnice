535e8e39b9a9de50c62e0f7dda92bf6b671c3047
Nikola Ninkovic <ninkovic.ra197.2018@uns.ac.rs>
2021-03-30 17:58:06 +0200

    Use case dijagram, prepravka.

--- HospitalApplication/HospitalApplication/obj/Debug/netcoreapp3.1/Windows/Doctor1/ShowOperationWindow.g.i.cs
+++ HospitalApplication/HospitalApplication/obj/Debug/netcoreapp3.1/Windows/Doctor1/ShowOperationWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace HospitalApplication.Windows.Doctor1 {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HospitalApplication;component/windows/doctor1/showoperationwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HospitalApplication;V1.0.0.0;component/windows/doctor1/showoperationwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\Windows\Doctor1\ShowOperationWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
