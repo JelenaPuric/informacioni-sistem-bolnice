3531e44de86e319f531a28e211ab185d8af38859
JelenaPuric <puricjelena99@gmail.com>
2021-03-31 02:50:19 +0200

    doktor ispravke u kodu

--- HospitalApplication/HospitalApplication/Windows/Doctor1/ScheduleExaminationWindow.xaml.cs
+++ HospitalApplication/HospitalApplication/Windows/Doctor1/ScheduleExaminationWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Logic;
+using Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
@@ -9,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WorkWithFiles;
 
 namespace HospitalApplication.Windows.Doctor1
 {
@@ -24,12 +27,51 @@ namespace HospitalApplication.Windows.Doctor1
 
         private void confirmBtn_Click(object sender, RoutedEventArgs e)
         {
+            SerializationAndDeserilaizationOfExaminations sAd = new SerializationAndDeserilaizationOfExaminations();
 
+            String patientsId = patientIdTxt.Text;
+            String DoctorId = doctorIdTxt.Text;
+            String RoomId = roomIdTxt.Text;
+            String ExaminationId = examIdTxt.Text;
+            string date = examDateTxt.Text;
+            string[] entries = date.Split('/');
+            int year = Int32.Parse(entries[2]);
+            int month = Int32.Parse(entries[0]);
+            int day = Int32.Parse(entries[1]);
+            DateTime date1 = new DateTime(year, month, day);
+            int d1 = Int32.Parse(hour1Txt.Text);
+            int d2 = Int32.Parse(minutes1Txt.Text);
+            int d3 = Int32.Parse(secundes1Txt.Text);
+            int d4 = Int32.Parse(hours2Txt.Text);
+            int d5 = Int32.Parse(minutes2Txt.Text);
+            int d6 = Int32.Parse(secundes2Txt.Text);
+            DateTime startDate = new DateTime(year, month, day, d1, d2, d3);
+            DateTime endDate = new DateTime(year, month, day, d4, d5, d6);
+            bool Specialization = (bool)specialistTxt.IsChecked;
+            String comboBox = typeExamTxt.Text;
+
+            int n = sAd.GetExaminations().Count;
+            int idExamination;
+
+            if (n > 0)
+            {
+                idExamination = Int32.Parse(sAd.GetExaminations()[n - 1].ExaminationId + 1);
+            }
+
+            else idExamination = 0;
+
+            ExaminationType typeOfExamination = (ExaminationType)Enum.Parse(typeof(ExaminationType), comboBox);
+
+            Examination exam = new Examination(patientsId, DoctorId, RoomId, startDate, endDate, date1, Specialization, idExamination.ToString(), typeOfExamination); ;
+            ExaminationAndOperationCrud eAo = new ExaminationAndOperationCrud();
+
+            eAo.ScheduleExamunation(exam);
+            Close();
         }
 
         private void giveUpBtn_Click(object sender, RoutedEventArgs e)
         {
-
+            Close();
         }
     }
 }
