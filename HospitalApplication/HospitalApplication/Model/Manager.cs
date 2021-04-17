24bd2f17b2b6e4afb565b2b3ef29fb4b77e292b2
kn3zzz <74219296+kn3zzz@users.noreply.github.com>
2021-04-17 21:01:20 +0200

    #bug pri otvaranju pacijenta

--- HospitalApplication/HospitalApplication/Model/Manager.cs
+++ HospitalApplication/HospitalApplication/Model/Manager.cs
@@ -2,48 +2,13 @@ using System;
 
 namespace Model
 {
-   public class Manager : Person
-   {
-      public System.Collections.ArrayList room;
-      
-      public System.Collections.ArrayList GetRoom()
-      {
-         if (room == null)
-            room = new System.Collections.ArrayList();
-         return room;
-      }
-      
-      public void SetRoom(System.Collections.ArrayList newRoom)
-      {
-         RemoveAllRoom();
-         foreach (Room oRoom in newRoom)
-            AddRoom(oRoom);
-      }
-      
-      public void AddRoom(Room newRoom)
-      {
-         if (newRoom == null)
-            return;
-         if (this.room == null)
-            this.room = new System.Collections.ArrayList();
-         if (!this.room.Contains(newRoom))
-            this.room.Add(newRoom);
-      }
-      
-      public void RemoveRoom(Room oldRoom)
-      {
-         if (oldRoom == null)
-            return;
-         if (this.room != null)
-            if (this.room.Contains(oldRoom))
-               this.room.Remove(oldRoom);
-      }
-      
-      public void RemoveAllRoom()
-      {
-         if (room != null)
-            room.Clear();
-      }
-   
-   }
+    public class Manager : Person
+    {
+        public Manager() { }
+
+        public Manager(string namee, string lastnamee, string idd, DateTime dateOfBirthh, string phoneNumberr, string emaill, string placeOfResidancee,
+            TypeOfPerson typeOfPersonn, string usernameee, string passworddd) : base(namee, lastnamee, idd, dateOfBirthh, phoneNumberr, emaill, placeOfResidancee,
+             typeOfPersonn, usernameee, passworddd)
+        { }
+    }
 }
