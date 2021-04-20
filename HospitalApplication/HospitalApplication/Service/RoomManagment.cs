264240b440860b35ecb704d7ae8ed3fc806132c2
kn3zzz <74219296+kn3zzz@users.noreply.github.com>
2021-04-20 03:32:58 +0200

    #upravnik - promena izlgeda stranica

--- HospitalApplication/HospitalApplication/Service/RoomManagment.cs
+++ HospitalApplication/HospitalApplication/Service/RoomManagment.cs
@@ -101,49 +101,5 @@ namespace Logic
 
             return r;
       }
-   
-      public System.Collections.ArrayList room;
-      
-      public System.Collections.ArrayList GetRoom()
-      {
-         if (room == null)
-            room = new System.Collections.ArrayList();
-         return room;
-      }
-      
-      public void SetRoom(System.Collections.ArrayList newRoom)
-      {
-         RemoveAllRoom();
-         foreach (Model.Room oRoom in newRoom)
-            AddRoom(oRoom);
-      }
-      
-      public void AddRoom(Model.Room newRoom)
-      {
-         if (newRoom == null)
-            return;
-         if (this.room == null)
-            this.room = new System.Collections.ArrayList();
-         if (!this.room.Contains(newRoom))
-            this.room.Add(newRoom);
-      }
-      
-      //public void RemoveRoom(Model.Room oldRoom)
-      //{
-      //   if (oldRoom == null)
-      //      return;
-      //   if (this.room != null)
-      //      if (this.room.Contains(oldRoom))
-      //         this.room.Remove(oldRoom);
-      //}
-      
-      public void RemoveAllRoom()
-      {
-         if (room != null)
-            room.Clear();
-      }
-   
-      private WorkWithFiles.SerializationAndDeserilazationOfRooms RoomLocation;
-   
    }
 }
