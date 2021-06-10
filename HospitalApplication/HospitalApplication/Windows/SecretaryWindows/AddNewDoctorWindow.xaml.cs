f92195a7fce81d61a4eaf844d038a124c96075d5
Nikola Ninkovic <ninkovic.ra197.2018@uns.ac.rs>
2021-06-10 03:04:00 +0200

    Implementirana validacija polja kod doktora

--- HospitalApplication/HospitalApplication/Windows/SecretaryWindows/AddNewDoctorWindow.xaml.cs
+++ HospitalApplication/HospitalApplication/Windows/SecretaryWindows/AddNewDoctorWindow.xaml.cs
@@ -3,6 +3,7 @@ using HospitalApplication.WorkWithFiles;
 using Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,6 +27,7 @@ namespace HospitalApplication.Windows.SecretaryWindows
         {
             InitializeComponent();
             CenterWindow();
+            MSex.IsChecked = true;
         }
 
         private void CenterWindow()
@@ -37,9 +39,111 @@ namespace HospitalApplication.Windows.SecretaryWindows
             this.Left = (screenWidth / 2) - (windowWidth / 2);
             this.Top = (screenHeight / 2) - (windowHeight / 2);
         }
-
+        // MessageBox.Show("Your examination is already scheduled at this time.", "Info", MessageBoxButton.OK);
         private void Submit_Click(object sender, RoutedEventArgs e)
         {
+
+            //VALIDACIJA PRAZNIH POLJA
+            if(ComboBox1.Text.Equals("") && textBoxFirstName.Text.Equals("") && textBoxLastName.Text.Equals("") && textBoxPhoneNumber.Text.Equals("") && textBoxEmail.Text.Equals("") &&
+                textBoxPlaceOfResidance.Text.Equals("") && textBoxUsername.Text.Equals("") && textBoxPassword.Text.Equals("") && textBoxJMBG.Text.Equals(""))
+            {
+                MessageBox.Show("All fields are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (ComboBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Type doctor field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxFirstName.Text.Equals(""))
+            {
+                MessageBox.Show("First name field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxLastName.Text.Equals(""))
+            {
+                MessageBox.Show("Last name field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxJMBG.Text.Equals(""))
+            {
+                MessageBox.Show("JMBG field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (BoxDateTime.Text.Equals(""))
+            {
+                MessageBox.Show("Date field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPlaceOfResidance.Text.Equals(""))
+            {
+                MessageBox.Show("Place of residance field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxEmail.Text.Equals(""))
+            {
+                MessageBox.Show("Email field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPhoneNumber.Text.Equals(""))
+            {
+                MessageBox.Show("Phone number field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxUsername.Text.Equals(""))
+            {
+                MessageBox.Show("Username field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPassword.Text.Equals(""))
+            {
+                MessageBox.Show("Password field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA First Name i Last name -- VELIKO POCETNO SLOVO
+            if (textBoxFirstName.Text[0].ToString() != textBoxFirstName.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of first name are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxLastName.Text[0].ToString() != textBoxLastName.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of last name are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA JMBG DA SU DOZVOLJENI BROJEVI SAMO I DA IMA 13 brojeva
+            if (!textBoxJMBG.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("In JMBG field only numbers are allowed", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxJMBG.Text.Length != 13)
+            {
+                MessageBox.Show("JMBG field must contain 13 numbers", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //VALIDACIJA Place of residance da je prvo veliko pocetno slovo
+            if (textBoxPlaceOfResidance.Text[0].ToString() != textBoxPlaceOfResidance.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of place of residance are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA Email adrese da bude u ispravnom obliku
+            if (!IsValidEmail(textBoxEmail.Text))
+            {
+                MessageBox.Show("Invalid email adress.", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //VALIDACIJA  Phone number da budu brojevi
+            if (!textBoxPhoneNumber.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("In phone number field only numbers are allowed", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //-------------------------------------------------------
+
+
+
             Doctor newDoctor = new Doctor((DoctorType)Enum.Parse(typeof(DoctorType), ComboBox1.Text), new List<DateTime>(), textBoxFirstName.Text, textBoxLastName.Text, GenerateIdForNewDoctor(),
                                           GetSelectedDate(), textBoxPhoneNumber.Text, textBoxEmail.Text, textBoxPlaceOfResidance.Text, 0, textBoxUsername.Text, textBoxPassword.Text, textBoxJMBG.Text,
                                           GetSelectedSexType());
@@ -47,6 +151,19 @@ namespace HospitalApplication.Windows.SecretaryWindows
             doctorService.CreateDoctor(newDoctor);
             allDoctorsWindow.UpdateDoctors();
             Close();
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private string GenerateIdForNewDoctor()
--- HospitalApplication/HospitalApplication/Windows/SecretaryWindows/EditDoctorWindow.xaml.cs
+++ HospitalApplication/HospitalApplication/Windows/SecretaryWindows/EditDoctorWindow.xaml.cs
@@ -3,6 +3,7 @@ using HospitalApplication.WorkWithFiles;
 using Model;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -92,11 +93,122 @@ namespace HospitalApplication.Windows.SecretaryWindows
         }
 
         private void Submit_Click(object sender, RoutedEventArgs e)
-        {
+        {      
+            //VALIDACIJA PRAZNIH POLJA
+            if (ComboBox1.Text.Equals("") && textBoxFirstName.Text.Equals("") && textBoxLastName.Text.Equals("") && textBoxPhoneNumber.Text.Equals("") && textBoxEmail.Text.Equals("") &&
+                textBoxPlaceOfResidance.Text.Equals("") && textBoxUsername.Text.Equals("") && textBoxPassword.Text.Equals("") && textBoxJMBG.Text.Equals(""))
+            {
+                MessageBox.Show("All fields are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (ComboBox1.Text.Equals(""))
+            {
+                MessageBox.Show("Type doctor field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxFirstName.Text.Equals(""))
+            {
+                MessageBox.Show("First name field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxLastName.Text.Equals(""))
+            {
+                MessageBox.Show("Last name field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxJMBG.Text.Equals(""))
+            {
+                MessageBox.Show("JMBG field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (BoxDateTime.Text.Equals(""))
+            {
+                MessageBox.Show("Date field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPlaceOfResidance.Text.Equals(""))
+            {
+                MessageBox.Show("Place of residance field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxEmail.Text.Equals(""))
+            {
+                MessageBox.Show("Email field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPhoneNumber.Text.Equals(""))
+            {
+                MessageBox.Show("Phone number field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxUsername.Text.Equals(""))
+            {
+                MessageBox.Show("Username field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxPassword.Text.Equals(""))
+            {
+                MessageBox.Show("Password field are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA First Name i Last name -- VELIKO POCETNO SLOVO
+            if (textBoxFirstName.Text[0].ToString() != textBoxFirstName.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of first name are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxLastName.Text[0].ToString() != textBoxLastName.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of last name are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA JMBG DA SU DOZVOLJENI BROJEVI SAMO I DA IMA 13 brojeva
+            if (!textBoxJMBG.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("In JMBG field only numbers are allowed", "Info", MessageBoxButton.OK);
+                return;
+            }
+            if (textBoxJMBG.Text.Length != 13)
+            {
+                MessageBox.Show("JMBG field must contain 13 numbers", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //VALIDACIJA Place of residance da je prvo veliko pocetno slovo
+            if (textBoxPlaceOfResidance.Text[0].ToString() != textBoxPlaceOfResidance.Text[0].ToString().ToUpper())
+            {
+                MessageBox.Show("First capital letter of place of residance are required", "Info", MessageBoxButton.OK);
+                return;
+            }
+            // VALIDACIJA Email adrese da bude u ispravnom obliku
+            if (!IsValidEmail(textBoxEmail.Text))
+            {
+                MessageBox.Show("Invalid email adress.", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //VALIDACIJA  Phone number da budu brojevi
+            if (!textBoxPhoneNumber.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("In phone number field only numbers are allowed", "Info", MessageBoxButton.OK);
+                return;
+            }
+            //-------------------------------------------------------
             SetNewValuesToSelectedDoctor();
             doctorService.UpdateDoctor(currentSelectedDoctor);
             allDoctorsWindow.UpdateDoctors();
             Close();
+        }
+
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
         private void Cancel_Click(object sender, RoutedEventArgs e)
